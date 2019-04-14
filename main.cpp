98f7eb275fcf7cc3187a52201fdb042d575f3062
unknown <tommasinistefano@gmail.com>
2019-04-14 14:26:12 +0200

    it

--- main.cpp
+++ main.cpp
@@ -73,8 +73,8 @@ int main(void) {
   gra.addNodo(2);
   gra.addNodo(3);
 
-  Grafo::const_iterator I = gra.begin();
-  Grafo::const_iterator E = gra.end();
+  Grafo<int>::const_iterator I = gra.begin();
+  Grafo<int>::const_iterator E = gra.end();
 
   for (; I != E; I++)
     std::cout << *I;
