864dc00f9ff752640aa18b0b5e2e4445120a1d8c
unknown <tommasinistefano@gmail.com>
2019-04-18 15:08:04 +0200

    funtori

--- main.cpp
+++ main.cpp
@@ -15,57 +15,6 @@ struct equal_int{
 	}
 };
 
-void testInt() {
-  Grafo<int> gi;
-  gi.addNodo(1);
-  gi.addNodo(2);
-  gi.addNodo(3);
-
-  gi.addEdge(1, 1);
-  gi.addEdge(2, 2);
-  gi.addEdge(3, 3);
-
-  std::cout << gi << std::endl;
-
-  std::cout << "RIMOZIONE NODO INTERMEDIO" << std::endl;
-  gi.delNodo(2);
-  std::cout << gi << std::endl;
-
-  std::cout << "RIMOZIONE NODO FINALE" << std::endl;
-  gi.delNodo(3);
-  std::cout << gi << std::endl;
-
-  std::cout << "RIMOZIONE NODO INIZIALE" << std::endl;
-  gi.delNodo(1);
-  std::cout << gi << std::endl;
-}
-
-void testChar() {
-  Grafo<char> gc;
-  gc.addNodo('A');
-  gc.addNodo('B');
-  gc.addNodo('C');
-
-  gc.addEdge('A', 'A');
-  gc.addEdge('B', 'B');
-  gc.addEdge('C', 'C');
-
-  std::cout << gc << std::endl;
-
-  std::cout << "RIMOZIONE NODO INTERMEDIO" << std::endl;
-  gc.delNodo('B');
-  std::cout << gc << std::endl;
-
-  std::cout << "RIMOZIONE NODO FINALE" << std::endl;
-  gc.delNodo('C');
-  std::cout << gc << std::endl;
-
-  std::cout << "RIMOZIONE NODO INIZIALE" << std::endl;
-  gc.delNodo('A');
-
-  std::cout << gc << std::endl;
-}
-
 void simpleTest() {
   int n = 3;
   Grafo<int, equal_int> gs(n);
