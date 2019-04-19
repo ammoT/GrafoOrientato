6acb259f8a7a35c9b0d589f8bfbe6235e2fb69d8
unknown <tommasinistefano@gmail.com>
2019-04-19 12:13:57 +0200

    more tests

--- main.cpp
+++ main.cpp
@@ -13,48 +13,15 @@ struct equal_String{
 	}
 };
 
-void testIt() {
-  Grafo<std::string, equal_String> gra;
-  gra.addNodo("UNO");
-  gra.addNodo("DUE");
-  gra.addNodo("TRE");
-  gra.addNodo("UNO");
-  std::cout << gra << std::endl;
-
-  Grafo<std::string, equal_String>::const_iterator i = gra.begin();
-  Grafo<std::string, equal_String>::const_iterator ie = gra.end();
-
-  std::cout << std::endl;
-  for (; i != ie; i++)
-      std::cout << *i;
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-
-}
-
-void simpleTest() {
-  int n = 3;
-  Grafo<int> gs(n);
-  for (int i = 1; i <= n; i++)
-    gs.addNodo(i);
-  for (int i = 1; i <= n; i++)
-    for (int j = 1; j <= n; j++)
-      gs.addEdge(i,j);
-  for (int i = 1; i <= n; i++)
-    gs.delEdge(i,i);
-	gs.addNodo(1);
-  std::cout << gs << std::endl;
-}
-
+void testIt();
 void testMetodiFondamentali();
 void testDelete();
+
 int main(void) {
   std::cout << "Grafo Orientato tramite matrici di adiacenza, Tommasini Stefano 810929" << std::endl;
 	testMetodiFondamentali();
 	testDelete();
   //testIt();
-  //simpleTest();
   return 0;
 }
 
@@ -103,6 +70,23 @@ void testDelete() {
 	gs.delEdge(1,3);
 	gs.delNodo(2);
 
-
 	std::cout << gs << std::endl;
+}
+
+void void testIt() {
+	Grafo<std::string, equal_String> gra;
+	gra.addNodo("UNO");
+	gra.addNodo("DUE");
+	gra.addNodo("TRE");
+	gra.addNodo("UNO");
+	std::cout << gra << std::endl;
+
+	Grafo<std::string, equal_String>::const_iterator i = gra.begin();
+	Grafo<std::string, equal_String>::const_iterator ie = gra.end();
+
+	std::cout << std::endl;
+	for (; i != ie; i++)
+		std::cout << *i;
+	std::cout << std::endl;
+	std::cout << std::endl;
 }
